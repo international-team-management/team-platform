724e7c22698e4ea184451cfc85281a6526543be2
Roman Makhov <nekromantos30888666@gmail.com>
2023-07-17 14:50:14 +0300

    feat: add routing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname),
+      'src': path.resolve(__dirname, 'src'),
       'assets': path.resolve(__dirname, 'src', 'assets'),
       'components': path.resolve(__dirname, 'src', 'components'),
       'pages': path.resolve(__dirname, 'src', 'pages'),
