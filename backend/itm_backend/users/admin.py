1034229cf9b79837e8d5693451f863cfa17791bd
Кирилл Астраханцев <picnichek1996@yandex.ru>
2023-07-18 22:43:55 +0500

    current_user_model_and_admin

--- backend/itm_backend/users/admin.py
+++ backend/itm_backend/users/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import User
+from .models import User, TimeTable
+
+class TimeTableInline(admin.TabularInline):
+    model = TimeTable
+    extra = 1
 
 class UserAdmin(admin.ModelAdmin):
     list_display = (
-        'email', 'first_name', 'last_name', 'role', 'created_at', 'update_at', 'is_active', 'user_timezone', 'timetable', 'photo', 'telephone_number'
+        'email', 'first_name', 'last_name', 'role', 'created_at', 'update_at', 'is_active', 'user_timezone', 'photo', 'telephone_number'
     )
+    inlines = [TimeTableInline]
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
