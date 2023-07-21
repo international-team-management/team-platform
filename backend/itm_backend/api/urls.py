ff1507885d20b9d6e065686ffbc3bc87c2ee390a
Pavel Morozov <filimon.doors@gmail.com>
2023-07-21 15:18:48 +0300

    add swaggerUI

---
+++ backend/itm_backend/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+]
--- backend/itm_backend/itm_backend/urls.py
+++ backend/itm_backend/itm_backend/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path("api/v1/", include("api.urls")),
     path("admin/", admin.site.urls),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
