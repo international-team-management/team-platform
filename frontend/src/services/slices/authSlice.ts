21f5cc248c2d5dbd135d2aab4952705904f37568
Timur Gain <timur.gain@gmail.com>
2023-07-23 01:55:29 +0400

    refactor: authSlice use cases comment

--- frontend/src/services/slices/authSlice.ts
+++ frontend/src/services/slices/authSlice.ts
@@ -3,6 +3,39 @@ import { authAPI } from 'services/api/authAPI';
 import type { RootState } from 'services/store';
 import type { RegisterRequestData, LoginRequestData } from 'services/api/types';
 
+// Сценарии
+
+// 1. Регистрация, onSubmit form
+//  (?)сохранить форму в redux обычным action (или держать данные в useState на случай отказа сервера)
+//  dispatch(register(...)) - из компонента отправить post-запрос, результат сохранить в store
+//    принять ответ сервера
+//      если auth/register/fulfilled
+//        в state.register... ставим метку об успехе регистрации
+//        в компонентах через useSelect, запускаем переадресацию на login или попап об успешной регистрации
+//      если auth/register/rejected
+//        в state.register... ставим метку об неудаче регистрации
+//        в компонентах через useSelect, запускаем попап о неудачной регистрации, и возможно передаем те данные в форму
+//
+// 2. Вход, onSubmit form
+//  (?)сохранить форму в redux обычным action (или держать данные в useState на случай отказа сервера)
+//  dispatch(login(...)) - из компонента отправить post-запрос, результат сохранить в store
+//    принять ответ сервера
+//      если auth/login/fulfilled
+//        в state.login... ставим isLoggedIn равным true
+//        action.payload.token сохранить в LocalStorage
+//      если auth/register/rejected
+//        в state.login... ставим isLoggedIn равным false
+//
+// 3. Выход, onClick btn
+//  dispatch(logout(...)) - из компонента отправить post-запрос, результат сохранить в store
+//    принять ответ сервера
+//      если auth/logout/fulfilled
+//        в state.login... ставим isLoggedIn равным false
+//        удалить токен из LocalStorage
+//      если auth/logout/rejected
+//        в компонентах через useSelect, сообщаем об ошибке выхода
+
+
 // Types
 type RegisterType = {
   user: RegisterRequestData | null;
@@ -48,7 +81,7 @@ const initialState: AuthStateType = {
 };
 
 
-// Async Thunks (action creators)
+// Thunks (async action creators)
 // createAsyncThunk simplifies our Redux app by returning an action creator
 // that dispatches promise lifecycle actions for us so we don't have to dispatch them ourselves.
 
@@ -57,7 +90,7 @@ export const register = createAsyncThunk(
   async (user: RegisterRequestData) => {
     try {
       const responseData = await authAPI.register(user);
-      return responseData                                                  //  <- ? promise, 'OK', или объект типа RegisterRequestData
+      return responseData                                               //  <- ? promise, 'OK', или объект типа RegisterRequestData
       // dispatch({type: 'auth/register/fulfilled', payload: responseData}) <- createAsyncThunk сам отправляет результат в store
     } catch {
       console.log('Error register')
@@ -104,7 +137,7 @@ export const authSlice = createSlice({
       .addCase(register.fulfilled, (state, action) => {
         state.register.isLoading = false;
         state.register.hasError = false;
-        // state.user = action.payload;  // <- ? Не понимаю как из payload достать объект типа RegisterRequestData
+        // state.register.user = action.payload;  // <- ? Не понимаю как из payload достать объект типа RegisterRequestData
       })
       .addCase(register.rejected, (state) => {
         state.register.isLoading = false;
@@ -145,5 +178,5 @@ export const authSlice = createSlice({
 
 
 // Selectors
-export const isRegister = (state: RootState) => state.auth.register.user // <- ? Из стейта нужно получать инфо, которая указывает на зарегался или нет
+export const selectRegisterStatus = (state: RootState) => state.auth.register.user // <- ? Из стейта нужно получать инфо, которая указывает на зарегался или нет
 export const selectAuthData = (state: RootState) => state.auth;
