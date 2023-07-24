daf03cef709d9c521773f9d45f600cd4a4b7ed9c
desm <desm80@yandex.ru>
2023-07-24 14:44:24 +0300

    Changed auth routes to api/vi/auth/

--- backend/itm_backend/itm_backend/urls.py
+++ backend/itm_backend/itm_backend/urls.py
@@ -4,6 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("api/v1/", include("api.urls")),
     path("admin/", admin.site.urls),
-    path("auth/", include("djoser.urls")),
-    path("auth/", include("djoser.urls.jwt")),
+    path("api/v1/auth/", include("djoser.urls")),
+    path("api/v1/auth/", include("djoser.urls.jwt")),
 ]
