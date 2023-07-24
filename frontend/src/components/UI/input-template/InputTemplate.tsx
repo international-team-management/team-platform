74e88a5bf01a60917feaf37b7c5e81e2aa290a8a
Roman Makhov <nekromantos30888666@gmail.com>
2023-07-24 12:02:48 +0300

    feat: add react-hook-form

--- frontend/src/components/UI/input-template/InputTemplate.tsx
+++ frontend/src/components/UI/input-template/InputTemplate.tsx
@@ -1,5 +1,4 @@
-import { nanoid } from 'nanoid';
-import React from 'react';
+import React, { MutableRefObject } from 'react';
 import styles from './InputTemplate.module.scss';
 import clsx from 'clsx';
 
@@ -11,26 +10,26 @@ type InputProps = {
   label: string,
   isValid?: boolean | undefined,
   isPassword?: boolean,
+  useTogglePassword?: boolean,
   placeholder?: string,
   helperText?: string,
   errorText?: string,
   value?: string,
-  innerRef?: unknown,
+  innerRef?: MutableRefObject<HTMLInputElement | null | undefined>,
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
   onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void,
   onClick?: (event: React.MouseEvent<HTMLInputElement>) => void,
   onToogle?: () => void
 }
 
-export const Input:React.FC<InputProps> = (props) => {
-  const inputId = nanoid();
+export const Input = (props:InputProps, ref: React.LegacyRef<HTMLInputElement> | undefined) => {
+  // const {ref, ...rest} = register(props.name);
 
   return (
     <div className={styles.input__wrapper}>
       <div className={styles.input__content}>
         <label
           className={styles.input__label}
-          htmlFor={inputId}
         >
           {props.label}
         </label>
@@ -42,17 +41,17 @@ export const Input:React.FC<InputProps> = (props) => {
               [styles.input__field_invalid]: props.isValid === false
             }
           )}
-          type={props.type}
+          // {...rest}
           name={props.name}
-          id={inputId}
-          value={props.value}
+          type={props.type}
           placeholder={props.placeholder || ''}
           onChange={props.onChange}
           onBlur={props.onBlur}
           onClick={props.onClick}
+          ref={ref}
         />
 
-        {props.isPassword &&
+        {(props.isPassword && props.useTogglePassword) &&
           <div
             className={clsx(
               styles.input__tooglePassword
@@ -82,3 +81,5 @@ export const Input:React.FC<InputProps> = (props) => {
     </div>
   )
 }
+
+export const MyInput = React.forwardRef(Input);
--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,56 +1,73 @@
-import { ChangeEvent, useState, useRef } from "react";
+import { ChangeEvent, useState } from "react";
 import { Link } from 'react-router-dom';
 import { ButtonTemplate } from "src/components/UI/button-template/ButtonTemplate";
-import { Input } from "src/components/UI/input-template/InputTemplate";
+import { MyInput } from "src/components/UI/input-template/InputTemplate";
 import { input } from "src/typings/constants";
 import { routes } from "src/routes";
 // import { helperTexts } from "src/utils/validation/helperTexts";
 import styles from './LoginPage.module.scss';
 import { TitleTemplate } from "src/components/UI/title-template/TitleTemplate";
+import { useForm } from "react-hook-form";
+import { LoginRequestData } from "src/services/api/types";
 
 export const LoginPage = () => {
-  const [emailValue, setEmailValue] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const password = useRef();
+  const [togglePassword, setTogglePassword] = useState(false);
 
-  const emailHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setEmailValue(value);
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: {errors}
+  } = useForm<LoginRequestData>();
 
   const showPasswordHandler = () => {
-    console.log('click')
     setShowPassword(!showPassword);
+  }
+
+  const handlerInputPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    const result = e.currentTarget.value;
+
+    (result && result.length > 0) ? setTogglePassword(true) : setTogglePassword(false);
+  }
+
+  const handlerFormSubmit = (data:LoginRequestData) => {
+    console.log(errors)
+    console.log(data)
   }
 
   return (
     <main className={styles.login}>
-      <form className={styles.login__wrapper}>
-        <TitleTemplate 
+      <form className={styles.login__wrapper} onSubmit={handleSubmit(handlerFormSubmit)}>
+        <TitleTemplate
           text='Вход'
         />
         <div className={styles.login__inputs}>
-          <Input
+          <MyInput
+            {...register(
+              'login',
+              {required: true}
+            )}
             type={input.EMAIL}
-            name='name'
             label='Email'
-            value={emailValue}
             placeholder='Введите email'
             helperText={''}
             isValid={undefined}
-            onChange={emailHandler}
           />
-          <Input
+          <MyInput
+            {...register(
+              'password',
+              {required: true}
+            )}
             type={!showPassword ? input.PASSWORD : input.TEXT}
-            name="password"
             label='Пароль'
             placeholder='Введите пароль'
             isPassword={true}
             isValid={undefined}
-            innerRef={password}
             onToogle={showPasswordHandler}
+            onChange={(e) => handlerInputPassword(e)}
+            useTogglePassword={togglePassword}
           />
-          <button className={styles.login__button}>Забыли пароль?</button>
+          <button className={styles.login__button} type="submit">Забыли пароль?</button>
         </div>
         <div className={styles.login__buttons}>
           <ButtonTemplate
