7b59856756fe68cff8f939a4fc4e21bae11641c2
Roman Makhov <nekromantos30888666@gmail.com>
2023-07-26 19:44:30 +0300

    Frontend feat validation (#41)
    * feat: add react-hook-form
    
    * feat: not anought work
    
    * feat: add full integrate rhf
    
    * fix: fix validation bugs
    
    * fix: checkValid

--- frontend/src/components/UI/input-template/InputTemplate.tsx
+++ frontend/src/components/UI/input-template/InputTemplate.tsx
@@ -1,9 +1,10 @@
-import { nanoid } from 'nanoid';
 import React from 'react';
 import styles from './InputTemplate.module.scss';
 import clsx from 'clsx';
 
-import {ReactComponent as Toogle} from 'assets/toogle.svg';
+import {ReactComponent as Eye} from 'assets/eye.svg';
+import {ReactComponent as EyeOff} from 'assets/eye-off.svg';
+import { input } from 'src/typings/constants';
 
 type InputProps = {
   type: string,
@@ -11,10 +12,16 @@ type InputProps = {
   label: string,
   labelPassword?: string,
   isValid?: boolean | undefined,
+  isEmpty?: boolean,
   isPassword?: boolean,
+  isToggle?: boolean,
+  useTogglePassword?: boolean,
   placeholder?: string,
   helperText?: string,
   errorText?: string,
+  register: any,
+  errors: any,
+  validOptions?: any,
   value?: string,
   innerRef?: unknown,
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void,
@@ -23,8 +30,71 @@ type InputProps = {
   onToogle?: () => void
 }
 
-export const Input:React.FC<InputProps> = (props) => {
-  const inputId = nanoid();
+// export const Input = (props:InputProps, ref: React.LegacyRef<HTMLInputElement> | undefined) => {
+//   // const {ref, ...rest} = register(props.name);
+
+//   return (
+//     <div className={styles.input__wrapper}>
+//       <div className={styles.input__content}>
+//         <label
+//           className={styles.input__label}
+//         >
+//           {props.label}
+//         </label>
+//         <input
+//           className={clsx(
+//             styles.input__field,
+//             {
+//               [styles.input__field_valid]: props.isValid === true,
+//               [styles.input__field_invalid]:  props.isValid === false
+//             }
+//           )}
+//           // {...rest}
+//           name={props.name}
+//           type={props.type}
+//           placeholder={props.placeholder || ''}
+//           onChange={props.onChange}
+//           onBlur={props.onBlur}
+//           onClick={props.onClick}
+//           ref={ref}
+//         />
+
+//         {(props.isPassword && props.useTogglePassword) &&
+//           <div
+//             className={clsx(
+//               styles.input__tooglePassword
+//             )}
+//             onClick={props.onToogle}
+//           >
+//             <Toogle />
+//           </div>
+//         }
+//       </div>
+//       {props.helperText &&
+//         <div className={clsx(
+//           styles.input__helperText,
+//           {
+//             [styles.input__helperText_valid]: props.isValid === true,
+//             [styles.input__helperText_invalid]: props.isValid === false
+//           }
+//         )}>
+//           {props.helperText}
+//         </div>
+//       }
+//       {(props.errorText && props.isValid === false) &&
+//         <div className={styles.input__errorText}>
+//           {props.errorText}
+//         </div>
+//       }
+//     </div>
+//   )
+// }
+
+// export const MyInput = React.forwardRef(Input);
+
+
+export const Input = (props:InputProps) => {
+  const checkValid = props.isValid || (props.isEmpty && !props.errors[props.name]);
 
   return (
     <div className={styles.input__wrapper}>
@@ -32,7 +102,6 @@ export const Input:React.FC<InputProps> = (props) => {
         <div className={styles.input__label_wrapper}>
           <label
             className={styles.input__label}
-            htmlFor={inputId}
           >
             {props.label}
           </label>
@@ -46,45 +115,66 @@ export const Input:React.FC<InputProps> = (props) => {
           className={clsx(
             styles.input__field,
             {
-              [styles.input__field_valid]: props.isValid === true,
-              [styles.input__field_invalid]: props.isValid === false
+              [styles.input__field_valid]: checkValid, //!props.errors[props.name] && !props.isEmpty,
+              [styles.input__field_invalid]: !!props.errors[props.name]
             }
           )}
+          // {...rest}
           type={props.type}
-          name={props.name}
-          id={inputId}
-          value={props.value}
           placeholder={props.placeholder || ''}
-          onChange={props.onChange}
-          onBlur={props.onBlur}
-          onClick={props.onClick}
+          {...props.register(
+            props.name,
+            props.validOptions
+          )}
         />
 
-        {props.isPassword &&
+        {(props.isPassword && props.useTogglePassword) &&
           <div
             className={clsx(
               styles.input__tooglePassword
             )}
             onClick={props.onToogle}
           >
-            <Toogle />
+            {props.isToggle ? <Eye /> : <EyeOff />}
           </div>
         }
       </div>
-      {props.helperText &&
-        <div className={clsx(
-          styles.input__helperText,
-          {
-            [styles.input__helperText_valid]: props.isValid === true,
-            [styles.input__helperText_invalid]: props.isValid === false
-          }
-        )}>
+      {(props.helperText && props.isEmpty && !props.errors[props.name]) &&
+        // props.helperText.map((item, index) => {
+        //   return (
+        //     <div
+        //       className={clsx(
+        //         styles.input__helperText,
+        //         {
+        //           [styles.input__helperText_valid]: props.isValid !== undefined && props.isValid,
+        //           [styles.input__helperText_invalid]: !!props.errors[props.name]
+        //         }
+        //       )}
+        //       key={index}
+        //     >
+        //       {item}
+        //     </div>
+        //   )
+        // })
+        <div
+          className={clsx(
+            styles.input__helperText,
+            props.name === input.PASSWORD &&  styles.input__helperText_password,
+            {
+              [styles.input__helperText_valid]: checkValid,
+              [styles.input__helperText_invalid]: !!props.errors[props.name]
+            }
+          )}
+        >
           {props.helperText}
         </div>
       }
-      {(props.errorText && props.isValid === false) &&
-        <div className={styles.input__errorText}>
-          {props.errorText}
+      {props.errors[props.name] &&
+        <div className={clsx(
+          styles.input__errorText,
+          props.name === input.PASSWORD && props.isEmpty && styles.input__errorText_password
+        )}>
+          {props.errors[props.name].message}
         </div>
       }
     </div>
--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,57 +1,116 @@
-import { ChangeEvent, useState, useRef } from "react";
+import { ChangeEvent, useState } from "react";
 import { Link } from 'react-router-dom';
 import { ButtonTemplate } from "src/components/UI/button-template/ButtonTemplate";
 import { Input } from "src/components/UI/input-template/InputTemplate";
 import { input } from "src/typings/constants";
 import { routes } from "src/routes";
-// import { helperTexts } from "src/utils/validation/helperTexts";
 import styles from './LoginPage.module.scss';
 import { TitleTemplate } from "src/components/UI/title-template/TitleTemplate";
+import { useForm } from "react-hook-form";
+import { LoginRequestData } from "src/services/api/types";
+import { DevTool } from "@hookform/devtools";
+import { errorTexts, helperTexts } from "src/utils/validation/helperTexts";
+import { patterns } from "src/utils/validation/patterns";
 import promo from '../../assets/Promo.png';
 
 export const LoginPage = () => {
-  const [emailValue, setEmailValue] = useState('');
   const [showPassword, setShowPassword] = useState(false);
-  const password = useRef();
+  const [togglePassword, setTogglePassword] = useState(false);
 
-  const emailHandler = (event: ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setEmailValue(value);
-  }
+  const [emptyPassword, setEmptyPassword] = useState(false);
+
+  const [isValidPassword, setIsValidPassword] = useState<boolean | undefined>(undefined);
+  const [isValidEmail, setIsValidEmail] = useState<boolean | undefined>(undefined);
+
+  const {
+    register,
+    reset,
+    control,
+    handleSubmit,
+    getFieldState,
+    formState: {errors}
+  } = useForm<LoginRequestData>(
+    {mode: 'onChange', criteriaMode: 'all'}
+  );
 
   const showPasswordHandler = () => {
-    console.log('click')
     setShowPassword(!showPassword);
+  }
+
+  const handlerInputPassword = (e: ChangeEvent<HTMLInputElement>) => {
+    const result = e.currentTarget.value.length;
+    // result ? setTogglePassword(true) : setTogglePassword(false);
+
+    if (result) {
+      setTogglePassword(true);
+      setEmptyPassword(true)
+    } else {
+      setTogglePassword(false);
+      setEmptyPassword(false)
+    }
+
+    setIsValidPassword(!getFieldState('password').invalid)
+  }
+
+  const handlerFormSubmit = (data:LoginRequestData) => {
+    console.log(data);
+    reset();
+  }
+
+  const handlerInputLogin = () => {
+    // const result = e.currentTarget.value.length;
+    // result ? setEmptyLogin(false) : setEmptyLogin(true);
+    setIsValidEmail(!getFieldState('login').invalid);
   }
 
   return (
     <main className={styles.login}>
       <img src={promo} className={styles.login__promo} alt="promo" />
-      <form className={styles.login__wrapper}>
+      <form className={styles.login__wrapper} onSubmit={handleSubmit(handlerFormSubmit)}>
         <TitleTemplate 
           text='С возвращением'
           descrption='Введите свои данные и войдите в аккаунт'
         />
         <div>
           <Input
+            register={register}
+            errors={errors}
+            validOptions={             {
+              onChange: () => handlerInputLogin(),
+              required: errorTexts.EMPTY_FIELD.PATTERN,
+              pattern: {
+                value: patterns.EMAIL,
+                message: errorTexts.EMAIL.PATTERN
+              }
+            }}
+            name={input.EMAIL}
             type={input.EMAIL}
-            name='name'
             label='Email'
-            value={emailValue}
             placeholder='example@site.mail'
-            helperText={''}
-            isValid={undefined}
-            onChange={emailHandler}
+            isValid={isValidEmail}
           />
           <Input
+            register={register}
+            errors={errors}
+            validOptions={              {
+              onChange: (e: ChangeEvent<HTMLInputElement>) => handlerInputPassword(e),
+              required: errorTexts.EMPTY_FIELD.PATTERN,
+              pattern: {
+                value: patterns.PASSWORD,
+                message: errorTexts.PASSWORD.PATTERN
+              }
+            }}
+            name={input.PASSWORD}
             type={!showPassword ? input.PASSWORD : input.TEXT}
-            name="password"
             label='Пароль'
             placeholder=''
-            isPassword={false}
-            isValid={undefined}
-            innerRef={password}
+            helperText={helperTexts.PASSWORD}
+            isPassword={true}
             onToogle={showPasswordHandler}
+            isToggle={!showPassword}
+            isEmpty={emptyPassword}
+            useTogglePassword={togglePassword}
+            isValid={isValidPassword}
             labelPassword='Забыли пароль?'
           />
         </div>
@@ -66,6 +125,7 @@ export const LoginPage = () => {
           </div>
         </div>
       </form>
+      <DevTool control={control} />
     </main>
   )
 }
