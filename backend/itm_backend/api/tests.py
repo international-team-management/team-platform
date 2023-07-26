52110dd5843f5197a82b6ae1e23f1e77b94c8b14
Евгений Лазаренков <lazarenkov88@gmail.com>
2023-07-26 15:17:03 +0300

    Test ver. 1.0

--- backend/itm_backend/api/tests.py
+++ backend/itm_backend/api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class StaticURLTests(TestCase):
+    def setUp(self):
+        self.guest_client = Client()
+
+    def test_schema_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/api/v1/schema/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_swaggerui_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/api/v1/swagger-ui/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_redoc_url_exists_at_desired_location(self):
+        response = self.guest_client.get('/api/v1/redoc/')
+        self.assertEqual(response.status_code, 200)
