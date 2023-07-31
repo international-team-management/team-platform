02547cd4c9cc3c5236efa6c7e3423aa3e402e142
Евгений Лазаренков <lazarenkov88@gmail.com>
2023-07-31 14:33:12 +0300

    V 2.0

--- backend/tests/fixtures/fixture_user.py
+++ backend/tests/fixtures/fixture_user.py
@@ -4,31 +4,11 @@ from rest_framework_simplejwt.tokens import AccessToken
 
 
 @pytest.fixture
-def user_superuser(django_user_model):
-    return django_user_model.objects.create_superuser(
-        email='superuser@yandex.ru',
-        username='TestSuperuser',
-        password="securepassword",
-        first_name="SuperUser",
-        last_name="Super",
-    )
-
-
-@pytest.fixture
 def token_user_superuser(user_superuser):
     token = AccessToken.for_user(user_superuser)
     return {
         'access': str(token),
     }
-
-
-@pytest.fixture
-def user_superuser_client(token_user_superuser):
-    client = APIClient()
-    client.credentials(
-        HTTP_AUTHORIZATION=f'Bearer {token_user_superuser["access"]}'
-    )
-    return client
 
 
 @pytest.fixture
@@ -42,32 +22,11 @@ def user(django_user_model):
 
 
 @pytest.fixture
-def another_user(django_user_model):
-    return django_user_model.objects.create_user(
-        email='testanother@mail.ru',
-        password='1234567',
-        first_name="Test2",
-        last_name="Testovich",
-    )
-
-
-"""@pytest.fixture
 def token(user):
     token = AccessToken.for_user(user)
     return {
         'access': str(token),
-    }"""
-
-@pytest.fixture
-def token(user):
-    from rest_framework_simplejwt.tokens import RefreshToken
-    refresh = RefreshToken.for_user(user)
-
-    return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
     }
-
 
 @pytest.fixture
 def user_client(token):
