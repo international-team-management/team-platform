0e85ea4e52240c1cff8b0e91f121819d00ed4f08
desm80 <desm80@yandex.ru>
2023-08-02 18:55:15 +0300

    model Project created

--- backend/itm_backend/projects/models.py
+++ backend/itm_backend/projects/models.py
@@ -1,3 +1,84 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Task(models.Model):
+    pass
+
+
+class Tag(models.Model):
+    """
+    Описание модели Tag.
+    """
+
+    name = models.CharField(max_length=200, unique=True, verbose_name="Название")
+
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Project(models.Model):
+    """
+    Описание модели Project.
+    """
+
+    STATUS_CHOICES = (
+        ("onbording", "Онбординг"),
+        ("in_progress", "В работе"),
+        ("production", "Проект взлетел"),
+        ("tests", "Тестирование"),
+    )
+
+    PRIORITY_CHOICES = (
+        ("maximum", "Максимальный"),
+        ("average", "Средний"),
+        ("minimum", "Минимальный"),
+        ("urgent", "Срочно"),
+    )
+
+    name = models.CharField(max_length=254, verbose_name="Название Проекта")
+    description = models.TextField(verbose_name="Описание Проекта")
+    owner = models.ForeignKey(User, on_delete=models.PROTECT, related_name="projects", verbose_name="Автор Проекта")
+    participants = models.ManyToManyField(User, through="ProjectUser", verbose_name="Участники проекта")
+    tasks = models.ManyToManyField(Task, related_name="projects", verbose_name="Задачи проекта")
+    deadline = models.TimeField(verbose_name="Время окончания проекта")
+    status = models.CharField(verbose_name="Статус проекта", choices=STATUS_CHOICES, max_length=20)
+    priority = models.CharField(verbose_name="Приоритет проекта", choices=PRIORITY_CHOICES, max_length=20)
+    tags = models.ManyToManyField(Tag, related_name="projects", verbose_name="Тэги")
+    created_at = models.DateTimeField(verbose_name="Дата регистрации проекта", auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name="Дата обновления проекта", auto_now=True)
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
+
+    def __str__(self):
+        return f"{self.name}, {self.description[::35]}"
+
+
+class ProjectUser(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.PROTECT, related_name="project_participants", verbose_name="Участник"
+    )
+    project_id = models.ForeignKey(
+        Project, on_delete=models.CASCADE, related_name="project_participants", verbose_name="Проект"
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=(
+                    "user_id",
+                    "project_id",
+                ),
+                name="unique_user_project",
+            ),
+        )
+        verbose_name = "Участник Проекта"
+        verbose_name_plural = "Участники Проекта"
