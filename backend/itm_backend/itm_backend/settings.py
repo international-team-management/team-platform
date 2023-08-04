bfae5bf0bdf8c31b9d7b2721d925f080f19dc922
desm <desm80@yandex.ru>
2023-08-04 08:28:01 +0300

    pre-commit done

--- backend/itm_backend/itm_backend/settings.py
+++ backend/itm_backend/itm_backend/settings.py
@@ -16,9 +16,9 @@ DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 #  https://pypi.org/project/django-cors-headers/
-#  CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:5173").split(",")
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "http://localhost:5173").split(",")
 
-#  CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = True
 
 INSTALLED_APPS = [
     "django.contrib.admin",
