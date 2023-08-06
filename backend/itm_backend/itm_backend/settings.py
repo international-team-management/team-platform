b5ba63ca963fcf1c8f16ad0b05afa8129c8b9ebf
Александр Рассыхаев <farexzon@yandex.ru>
2023-08-06 13:43:59 +0300

    git-workflow fix v3.12

--- backend/itm_backend/itm_backend/settings.py
+++ backend/itm_backend/itm_backend/settings.py
@@ -99,14 +99,45 @@ WSGI_APPLICATION = "itm_backend.wsgi.application"
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.sqlite3"),
+#         "NAME": os.getenv("POSTGRES_DB", default=(BASE_DIR / "db.sqlite3")),
+#         "USER": os.getenv("POSTGRES_USER", default="postgres"),
+#         "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
+#         "HOST": os.getenv("DB_HOST", default="localhost"),
+#         "PORT": os.getenv("DB_PORT", default=5432),
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.sqlite3"),
-        "NAME": os.getenv("POSTGRES_DB", default=(BASE_DIR / "db.sqlite3")),
-        "USER": os.getenv("POSTGRES_USER", default="postgres"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
-        "HOST": os.getenv("DB_HOST", default="localhost"),
-        "PORT": os.getenv("DB_PORT", default=5432),
+    'default': {
+        'ENGINE': os.getenv(
+            'DB_ENGINE',
+            default="django.db.backends.postgresql"
+        ),
+        'NAME': os.getenv(
+            'POSTGRES_DB',
+            default="postgres"
+        ),
+        'USER': os.getenv(
+            'POSTGRES_USER',
+            default="postgres"
+        ),
+        'PASSWORD': os.getenv(
+            'POSTGRES_PASSWORD',
+            default="sasha777"
+        ),
+        'HOST': os.getenv(
+            'DB_HOST',
+            default="db"
+        ),
+        'PORT': os.getenv(
+            'DB_PORT',
+            default="5432"
+        ),
+        'OPTIONS': {'options': '-c timezone=UTC'},
     }
 }
 
