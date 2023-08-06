9419f2d888412de0cd9f2597cfcd66f358a64550
Александр Рассыхаев <farexzon@yandex.ru>
2023-08-06 14:53:18 +0300

    git-workflow fix v3.17

--- backend/itm_backend/itm_backend/settings.py
+++ backend/itm_backend/itm_backend/settings.py
@@ -154,6 +154,10 @@ STATIC_URL = "/static/"
 
 STATIC_ROOT = BASE_DIR / "static"
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / "media"
