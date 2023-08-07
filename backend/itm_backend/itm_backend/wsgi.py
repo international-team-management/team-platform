6c54de8f3403036a66da7808dbadb7b216379031
Александр Рассыхаев <farexzon@yandex.ru>
2023-08-07 21:14:17 +0300

    Server test: backend+frontend v1.14

--- backend/itm_backend/itm_backend/wsgi.py
+++ backend/itm_backend/itm_backend/wsgi.py
@@ -12,8 +12,6 @@ import sys
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "itm_backend.settings")
 
 application = get_wsgi_application()
