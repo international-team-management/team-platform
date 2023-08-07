166f35b02001519cf15251d6ac248945a931bbda
Александр Рассыхаев <farexzon@yandex.ru>
2023-08-07 17:07:20 +0300

    Server test: backend+frontend v1.04

--- backend/itm_backend/itm_backend/settings.py
+++ backend/itm_backend/itm_backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
