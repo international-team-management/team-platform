d860f0cbbc8f9351bea34d67b308a8d127e8c63c
Александр Рассыхаев <farexzon@yandex.ru>
2023-08-08 22:25:18 +0300

    Server test: backend+frontend v1.27

--- backend/itm_backend/itm_backend/settings.py
+++ backend/itm_backend/itm_backend/settings.py
@@ -16,9 +16,7 @@ DEBUG = True
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 #  https://pypi.org/project/django-cors-headers/
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+CORS_ALLOWED_ORIGINS = []
 
 CORS_ALLOW_CREDENTIALS = True
 
