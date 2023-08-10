bcb1825272db7ffe2ccae13ec8fa5b4119c81a94
Данил Кочетов <dmi194@yandex.ru>
2023-08-10 15:55:48 +0300

    added to_representation to CustomUserCreateSerializer

--- backend/itm_backend/users/serializers.py
+++ backend/itm_backend/users/serializers.py
@@ -46,6 +46,17 @@ class CustomUserCreateSerializer(serializers.ModelSerializer):
         validated_data["password"] = make_password(validated_data["password"])
         return super().create(validated_data)
 
+    def to_representation(self, instance):
+        """
+        Возвращаем ответ по ТЗ.
+        """
+        return {
+            "id": instance.id,
+            "email": instance.email,
+            "first_name": instance.first_name,
+            "last_name": instance.last_name
+        }
+
 
 class CustomUserSerializer(serializers.ModelSerializer):
     """
