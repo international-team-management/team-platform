860f087b79a9a020b75b24c915056e2f16dc9422
Roman Makhov <nekromantos30888666@gmail.com>
2023-08-14 11:27:41 +0300

    Frontend feat avatar (#159)
    * feat: add new avatar with pallete
    
    * refactor: stylelint
    
    * feat: add encode img to Base64

--- frontend/src/typings/constants.ts
+++ frontend/src/typings/constants.ts
@@ -3,6 +3,7 @@ export enum InputType {
   PASSWORD = 'password',
   EMAIL = 'email',
   SELECT = 'select',
+  FILE = 'file',
 }
 
 export enum InputName {
@@ -19,4 +20,5 @@ export enum InputName {
   WORK_START = 'work_start',
   WORK_FINISH = 'work_finish',
   TELEPHONE = 'telephone_number',
+  PHOTO = 'photo',
 }
