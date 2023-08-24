7c23909834f1ffd2da0d81d21355320584f2647c
Roman Makhov <nekromantos30888666@gmail.com>
2023-08-24 15:56:01 +0300

    feat: add squad header

--- frontend/src/components/team-area/TeamArea.tsx
+++ frontend/src/components/team-area/TeamArea.tsx
@@ -1,6 +1,30 @@
 import clsx from 'clsx';
 import styles from './TeamArea.module.scss';
 
+import { ReactComponent as FilterIcon } from 'assets/icon-filter-members.svg';
+
 export const TeamArea = (): JSX.Element => {
-  return <div className={clsx('team__element', styles.squad)}>TeamArea</div>;
+  return (
+    <div className={clsx('team__element', styles.squad)}>
+      <div className={styles.squad__header}>
+        <div className={styles['squad__full-team']}>
+          <input type="checkbox" name="full_team" id="full_team" value="all" />
+          <label htmlFor="full_team">Все участники</label>
+        </div>
+        <div className={styles.squad__search}>
+          <div className={styles['squad__input-wrapper']}>
+            <input
+              type="text"
+              name="search__teammate"
+              id="search__teammate"
+              placeholder="Найти участника"
+            />
+          </div>
+          <div className={styles.squad__filter}>
+            <FilterIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
