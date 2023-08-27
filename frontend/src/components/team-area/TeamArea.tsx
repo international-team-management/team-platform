95df3202fa4655766689e184384ec8a011bdbeba
Roman Makhov <nekromantos30888666@gmail.com>
2023-08-27 16:20:07 +0300

    feat: add teammate

--- frontend/src/components/team-area/TeamArea.tsx
+++ frontend/src/components/team-area/TeamArea.tsx
@@ -2,13 +2,28 @@ import clsx from 'clsx';
 import styles from './TeamArea.module.scss';
 
 import { ReactComponent as FilterIcon } from 'assets/icon-filter-members.svg';
+import { Teammate } from '../team-teammate/Teammate';
+import { TeamMock } from './team-mock';
+import { useState } from 'react';
 
 export const TeamArea = (): JSX.Element => {
+  const [isAllChecked, setIsAllChecked] = useState(false);
+
+  const handlerAllChecked = () => {
+    setIsAllChecked(!isAllChecked);
+  };
+
   return (
     <div className={clsx('team__element', styles.squad)}>
       <div className={styles.squad__header}>
         <div className={styles['squad__full-team']}>
-          <input type="checkbox" name="full_team" id="full_team" value="all" />
+          <input
+            type="checkbox"
+            name="full_team"
+            id="full_team"
+            value="all"
+            onClick={handlerAllChecked}
+          />
           <label htmlFor="full_team">Все участники</label>
         </div>
         <div className={styles.squad__search}>
@@ -24,6 +39,24 @@ export const TeamArea = (): JSX.Element => {
             <FilterIcon />
           </div>
         </div>
+      </div>
+      <div className={styles.squad__table}>
+        <div className={styles['squad__table-header']}>
+          <span>Участник</span>
+          <span>Контакты</span>
+          <span>График работы</span>
+        </div>
+        {TeamMock.map((item) => (
+          <Teammate
+            name={item.name}
+            email={item.email}
+            jobTitle={item.jobTitle}
+            phone={item.phone}
+            time={item.time}
+            key={item.email}
+            isAllChecked={isAllChecked}
+          />
+        ))}
       </div>
     </div>
   );
--- frontend/src/pages/TeamPage/TeamPage.tsx
+++ frontend/src/pages/TeamPage/TeamPage.tsx
@@ -4,13 +4,12 @@ import clsx from 'clsx';
 import styles from './TeamPage.module.scss';
 import { TeamArea } from 'src/components/team-area/TeamArea';
 import { TeamIntersections } from 'src/components/team-intersections/TeamIntersections';
-import { TeamForm } from 'src/components/team-form/TeamForm';
 
 export const TeamPage = (): JSX.Element => {
   return (
     <section className={clsx('content', styles.team)}>
       <TeamIntersections />
-      <TeamForm />
+      <AddMember />
       <TeamArea />
     </section>
   );
