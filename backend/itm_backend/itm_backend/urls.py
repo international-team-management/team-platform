98278f55be4ec4b21c341c7e71050a3d98fe24e7
desm <desm80@yandex.ru>
2023-08-29 08:54:19 +0300

    Serving out media content to frontend added in urls.py Added preview of uploaded avatar in admin panel

--- backend/itm_backend/itm_backend/urls.py
+++ backend/itm_backend/itm_backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -7,3 +9,6 @@ urlpatterns = [
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("djoser.urls.jwt")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
