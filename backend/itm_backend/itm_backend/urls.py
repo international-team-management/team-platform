24736a2c3c2d03297b78f7422f2eee1ab4e63680
Roman Makhov <nekromantos30888666@gmail.com>
2023-09-02 20:40:40 +0300

    feat: without backend

--- backend/itm_backend/itm_backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("api/v1/", include("api.urls")),
-    path("admin/", admin.site.urls),
-    path("api/v1/auth/", include("djoser.urls")),
-    path("api/v1/auth/", include("djoser.urls.jwt")),
-]
