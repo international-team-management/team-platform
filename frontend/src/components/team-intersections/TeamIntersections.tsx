ba1d9c8dda694f690a7af85ea1a05e63cf6929a7
Roman Makhov <nekromantos30888666@gmail.com>
2023-09-07 08:51:54 +0300

    feat: add intersections component, fix avatar

--- frontend/src/components/team-intersections/TeamIntersections.tsx
+++ frontend/src/components/team-intersections/TeamIntersections.tsx
@@ -1,10 +1,17 @@
 import clsx from 'clsx';
 import styles from './TeamIntersections.module.scss';
+import { TeamIntersectionsComponent } from '../team-intersections-component/TeamIntersectionsComponent';
 
 export const TeamIntersections = (): JSX.Element => {
   return (
     <div className={clsx('team__element', styles.intersections)}>
-      Team Intersections
+      <h3 className={styles.intersections__title}>Пересечение по времени</h3>
+      <TeamIntersectionsComponent
+        width={'372px'}
+        background={'red'}
+        total={9}
+        time={'10:00-12:00'}
+      />
     </div>
   );
 };
--- frontend/src/typings/app.d.ts
+++ frontend/src/typings/app.d.ts
@@ -2,4 +2,5 @@ export global {
   export declare type Nullable<T> = T | null;
 
   export declare const __API_ENDPOINT__: string; // это нужно, чтобы потом добавить переменную в .env и TS не ругался
+  export declare const __API_ENDPOINT_BASE__: string;
 }
